48e860deea12e7fcde0e345c8c1549bc9e3c790c
DavAug <david.augustin@gmx.net>
2020-08-27 16:32:23 +0200

    finished PharmacodynamicModel class

--- pkpd/_models.py
+++ pkpd/_models.py
@@ -6,282 +6,127 @@
 #
 
 import myokit
+import myokit.formats.sbml as sbml
 import numpy as np
 import pints
 
 
-class TumourGrowthModel(pints.ForwardModel):
-    r"""
-    Creates a `pints.ForwardModel` which wraps a tumour growth model of the
-    form
-
-    .. math::
-        \frac{\text{d}V^s_T}{\text{d}t} =
-        \frac{2\lambda _0\lambda _1 V^s_T}{2\lambda _0 V^s_T + \lambda _1}.
-
-    Here, :math:`V^s_T` is the predicted tumour volume by the structural model,
-    :math:`\lambda _0` is the exponential growth rate, and :math:`\lambda _1`
-    is the linear growth rate.
-
-    If the flag `dimless` is `True` the dimensionless representation
-
-    .. math::
-        \frac{\text{d}v}{\text{d}\tau} =
-        \frac{a_1v}{v + a_0},
-
-    is insantiated instead, where (:math:`v`, :math:`\tau `) are related to
-    (:math:`V^s_T`, :math:`t`) by some characteristic volume and time scale
-    (:math:`V^c`, :math:`t^c`).
-
-    The model is parameterised by either (:math`V_0`, :math:`\lambda _0`,
-    :math:`\lambda _1`) or (:math`v_0`, :math:`a _0`, :math:`a _1`) which
-    are related by
-
-    .. math::
-        (V_0, \lambda _0, \lambda _1) =
-        \left( v_0\, V^c_T, \frac{a_1}{2a_0}\frac{1}{t^c},
-        a_1 \frac{V^c_T}{t^c} \right) .
+class PharmacodynamicModel(pints.ForwardModel):
+    """
+    Creates a `pints.ForwardModel` from a SBML model.
 
     Arguments:
-        is_treated -- Boolean flag indicating whether the tumour is simulated
-                      in absence or in presence of treatment.
-        dimless -- Boolean flag indicating whether the model is used in its
-                   dimensionfull or dimensionless representation.
-        log_transformed -- Boolean flag which indicates whether the model
-                           parameters are assumed to be log-transformed.
+        path -- Absolute path to SBML model file.
+        is_log_transformed -- Flag whether model parameters are
+                              log-transformed.
     """
-    def __init__(self, is_treated=False, dimless=False, log_transformed=False):
-        super(TumourGrowthModel, self).__init__()
 
-        if is_treated:
-            raise NotImplementedError
+    def __init__(self, path, is_log_transformed=True):
+        super(PharmacodynamicModel, self).__init__()
 
-        self._dimless = dimless
-        self._log_transformed = log_transformed
+        model = sbml.SBMLImporter().model(path)
+        self._is_log_transformed = is_log_transformed
 
-        if dimless:
-            self._model = self._create_dimless_tumour_growth_model()
-        else:
-            self._model = self._create_tumour_growth_model()
+        # Get the number of states and parameters
+        self._n_states = model.count_states()
+        n_const = model.count_variables(const=True)
+        self._n_parameters = self._n_states + n_const
+
+        # Get iterator over constant variables and states
+        self._state_names = [var.qname() for var in model.states()]
+        self._const_names = [
+            var.qname() for var in model.variables(const=True)]
+
+        # Set default outputs
+        self._output_names = self._state_names
+        self._n_outputs = self._n_states
 
         # Create simulator
-        self._sim = myokit.Simulation(self._model)
+        self._sim = myokit.Simulation(model)
 
-    def n_parameters(self):
+    def _set_const(self, parameters):
         """
-        Returns the number of parameters to fit. Either the initial tumour
-        volume, the exponential tumour growth and the linear tumour growth, or
-        the three equivalent dimensionless parameters.
+        Sets values of constant model parameters.
         """
-        return 3
+        for id_var, var in enumerate(self._const_names):
+            self._sim.set_constant(var, float(parameters[id_var]))
 
     def n_outputs(self):
         """
-        Returns the number of output dimensions. Here 1 (the tumour volume).
+        Returns the number of output dimensions.
+
+        By default this is the number of states.
         """
-        return 1
+        return self._n_outputs
+
+    def n_parameters(self):
+        """
+        Returns the number of parameters in the model.
+
+        Parameters of the model are initial state values and structural
+        parameter values.
+        """
+        return self._n_parameters
+
+    def outputs(self):
+        """
+        Returns the output names of the model.
+        """
+        return self._output_names
+
+    def parameters(self):
+        """
+        Returns the parameter names of the model.
+        """
+        return self._state_names + self._const_names
+
+    def set_outputs(self, outputs):
+        """
+        Sets outputs of the model.
+
+        Outputs has to be a list of quantifiable variable names of the
+        myokit.Model, e.g. `compartment.variable`.
+        """
+        # Check that outputs are valid
+        for output in outputs:
+            try:
+                self._sim._model.get(output)
+            except KeyError:
+                raise KeyError(
+                    'The variable <' + str(output) + '> does not exist in the '
+                    'model.')
+
+        self._output_names = outputs
+        self._n_outputs = len(outputs)
 
     def simulate(self, parameters, times):
         """
-        Solve the system of ODEs numerically for given parameters.
-
-        Parameters are assumed to be ordered as follows:
-        Dimensionfull model:
-            [initial tumour volume, exp. growth rate, lin. growth rate]
-        Dimensionless model:
-            [initial tumour volume, a_0, a_1].
+        Returns the numerical solution of the model outputs for specified
+        parameters and times.
         """
         # Reset simulation
         self._sim.reset()
 
-        # Sort input parameters
-        initial_state, param_0, param_1 = np.exp(parameters) if \
-            self._log_transformed else parameters
+        # Transform parameters back to linear scale if log-transfomed
+        if self._is_log_transformed:
+            parameters = np.exp(parameters)
 
-        # Set initial condition
-        self._sim.set_state([initial_state])
+        # Set initial conditions
+        self._sim.set_state(parameters[:self._n_states])
 
-        # Set growth constants
-        if self._dimless:
-            self._sim.set_constant('central.a_0', param_0)
-            self._sim.set_constant('central.a_1', param_1)
-        else:
-            self._sim.set_constant('central.lambda_0', param_0)
-            self._sim.set_constant('central.lambda_1', param_1)
-
-        # Define logged variable
-        loggedVariable = 'central.volume_t'
+        # Set constant model parameters
+        self._set_const(parameters[self._n_states:])
 
         # Simulate
         output = self._sim.run(
-            times[-1] + 1, log=[loggedVariable], log_times=times)
-        result = output[loggedVariable]
+            times[-1] + 1, log=self._output_names, log_times=times)
+        result = [output[name] for name in self._output_names]
 
-        return np.array(result)
+        # Transform shape of output to be compatible with
+        # pints.SingleOutputProblem/pints.MultiOutputProblem
+        if self._n_outputs == 1:
+            result = np.array(result).flatten()
+        else:
+            result = np.array(result).transpose()
 
-    def _create_dimless_tumour_growth_model(self):
-        r"""
-        Returns a non-dimensionalised tumour growth myokit model.
-
-        .. math::
-            \frac{\text{d}v}{\text{d}\tau} = \frac{a_1 v}
-            {v + a_0},
-
-        where the tumour volume :math:`v` and time :math:`\tau` are
-        dimensionless and measured in characteristic scales :math:`V^c_T` and
-        :math:`t^c`. The model parameters :math:`a_0` and :math:`a_1` are also
-        dimensionless.
-        """
-        # Instantiate model
-        model = myokit.Model()
-
-        # Add central compartment
-        central_comp = model.add_component('central')
-
-        # Add tumour growth variables to central compartment
-        volume_t = central_comp.add_variable('volume_t')
-        a_0 = central_comp.add_variable('a_0')
-        a_1 = central_comp.add_variable('a_1')
-
-        # Bind time
-        time = central_comp.add_variable('time')
-        time.set_binding('time')
-
-        # Set intial values (some default values) and units
-        time.set_rhs(0)
-
-        volume_t.set_rhs(0)
-        a_0.set_rhs(1)  # Avoid ZeroDivisionError
-        a_1.set_rhs(0)
-
-        # Set units
-        time.set_unit('dimensionless')
-
-        volume_t.set_unit('dimensionless')
-        a_0.set_unit('dimensionless')
-        a_1.set_unit('dimensionless')
-
-        # Set rhs of tumor volume
-        # dot(volume_t) =
-        #  (a_1 * volume_t) /
-        #  (volume_t + a_0)
-        volume_t.promote()
-        volume_t.set_rhs(
-            myokit.Divide(
-                myokit.Multiply(
-                    myokit.Name(a_1),
-                    myokit.Name(volume_t)
-                ),
-                myokit.Plus(
-                    myokit.Name(volume_t),
-                    myokit.Name(a_0)
-                )
-            )
-        )
-
-        # Validate model
-        model.validate()
-
-        # Check units
-        model.check_units()
-
-        return model
-
-    def _create_tumour_growth_model(self):
-        r"""
-        Returns a tumour growth myokit model.
-
-        .. math::
-            \frac{\text{d}V^s_T}{\text{d}t} =
-            \frac{2\lambda _0\lambda _1 V^s_T}
-            {2\lambda _0 V^s_T + \lambda _1},
-
-        where the tumour volume :math:`V^s_T` is measured in
-        :math:`\text{cm}^3`,
-        the exponential growth rate :math:`\lambda _0` is mesured in
-        :math:`\text{day}` and the linear growth rate :math:`\lambda _1` is
-        measured in :math:`\text{cm}^3/\text{day}`.
-        """
-        # Instantiate model
-        model = myokit.Model()
-
-        # add central compartment
-        central_comp = model.add_component('central')
-
-        # add tumour growth variables to central compartment
-        volume_t = central_comp.add_variable('volume_t')
-        lambda_0 = central_comp.add_variable('lambda_0')
-        lambda_1 = central_comp.add_variable('lambda_1')
-
-        # bind time
-        time = central_comp.add_variable('time')
-        time.set_binding('time')
-
-        # set preferred representation of units
-        # time in days
-        unit = myokit.parse_unit('day')
-        myokit.Unit.register_preferred_representation('day', unit)
-        # rates in 1 / day
-        unit = myokit.parse_unit('1/day')
-        myokit.Unit.register_preferred_representation('1/day', unit)
-        # tumor volume
-        unit = myokit.parse_unit('cm^3')
-        myokit.Unit.register_preferred_representation('cm^3', unit)
-        # linear growth
-        unit = myokit.parse_unit('cm^3/day')
-        myokit.Unit.register_preferred_representation('cm^3/day', unit)
-
-        # set intial values (some default values) and units
-        time.set_rhs(0)
-
-        volume_t.set_rhs(0)
-        lambda_0.set_rhs(0)
-        lambda_1.set_rhs(1)  # avoid ZeroDivisionError
-
-        # set units
-        time.set_unit('day')  # time in days
-
-        volume_t.set_unit('cm^3')  # milimeter cubed
-        lambda_0.set_unit('1 / day')  # per day
-        lambda_1.set_unit('cm^3 / day')  # milimiter cubed per day
-
-        # set rhs of tumor volume
-        # dot(volume_t) =
-        #  (2 * lambda_0 * lambda_1 * volume_t) /
-        #  (2 * lambda_0 * volume_t + lambda_1)
-        volume_t.promote()
-        volume_t.set_rhs(
-            myokit.Divide(
-                myokit.Multiply(
-                    myokit.Number(2),
-                    myokit.Multiply(
-                        myokit.Name(lambda_0),
-                        myokit.Multiply(
-                            myokit.Name(lambda_1),
-                            myokit.Name(volume_t)
-                        )
-                    )
-                ),
-                myokit.Plus(
-                    myokit.Multiply(
-                        myokit.Number(2),
-                        myokit.Multiply(
-                            myokit.Name(lambda_0),
-                            myokit.Name(volume_t)
-                        )
-                    ),
-                    myokit.Name(lambda_1)
-                )
-            )
-        )
-
-        # Validate model
-        model.validate()
-
-        # TODO: Check units
-        # model.check_units()
-
-        return model
-
-    def __str__(self):
-        return self._model.code()
+        return result
