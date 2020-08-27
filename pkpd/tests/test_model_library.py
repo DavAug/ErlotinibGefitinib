21e0e6e605b2c443cfea4f95c039ee36ed125342
DavAug <david.augustin@gmx.net>
2020-08-27 12:15:12 +0200

    add model validity tests

--- pkpd/tests/test_model_library.py
+++ pkpd/tests/test_model_library.py
@@ -8,6 +8,9 @@
 import os.path
 import unittest
 
+import myokit
+import myokit.formats.sbml as sbml
+
 import pkpd
 
 
@@ -35,6 +38,25 @@ class TestModelLibrary(unittest.TestCase):
         self.assertTrue(os.path.exists(path))
         path = self.model_library.get_path(models[2])
         self.assertTrue(os.path.exists(path))
+
+    def test_model_validity(self):
+        importer = sbml.SBMLImporter()
+        models = self.model_library.models()
+
+        # Test model 1
+        path = self.model_library.get_path(models[0])
+        myokit_model = importer.model(path)
+        self.assertIsInstance(myokit_model, myokit.Model)
+
+        # Test model 2
+        path = self.model_library.get_path(models[1])
+        myokit_model = importer.model(path)
+        self.assertIsInstance(myokit_model, myokit.Model)
+
+        # Test model 3
+        path = self.model_library.get_path(models[2])
+        myokit_model = importer.model(path)
+        self.assertIsInstance(myokit_model, myokit.Model)
 
 
 if __name__ == '__main__':
