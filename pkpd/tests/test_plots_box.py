0af32a07dae66741cde68fdd61f132b894e53525
DavAug <david.augustin@gmx.net>
2020-08-28 14:37:15 +0200

    copyright

--- pkpd/tests/test_plots_box.py
+++ pkpd/tests/test_plots_box.py
@@ -1,4 +1,4 @@
-
+#
 # This file is part of the ErlotinibGefitinib repository
 # (https://github.com/DavAug/ErlotinibGefitinib/) which is released under the
 # BSD 3-clause license. See accompanying LICENSE.md for copyright notice and
